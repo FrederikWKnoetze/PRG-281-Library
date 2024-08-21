d384514e1ad17234ef7a9ce43309844d10c53e13
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-08-21 19:26:57 +0200

    addding

---
+++ Library/Library/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int TempID = random.Next(10000,99999);
+                Reader Test = new Reader(TempID.ToString(),"Test");
+                
+            }
+            Reader Gideon = new Reader("12345", "Gideon");
+            foreach (Reader reader in Reader.AllReaders)
+            {
+                string testID = textBox1.Text;
+                string testName = textBox2.Text;
+
+                if (testID == reader.ID && testName == reader.Name)
+                {
+
+                    MessageBox.Show($"ID: {reader.ID}, Name: {reader.Name}");
+                }
+            }
+        }
+    }
+}
---
+++ Library/Library/Reader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Reader
+    {
+        private string id;
+        private string name;
+        private List<Book> books;
+        private static List<Reader> allReaders = new List<Reader>();
+
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+        public static List<Reader> AllReaders
+        {
+            get { return allReaders; }
+        }
+        public Reader(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.books = new List<Book>();
+            allReaders.Add(this);
+        }
+    }
+}
