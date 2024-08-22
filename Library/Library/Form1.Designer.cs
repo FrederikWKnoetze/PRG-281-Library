7aa9007d2a6e17a0f2a4cc24063e0a2513542945
Artix <kwevoel727@gmail.com>
2024-08-22 16:36:57 +0200

    Login Page

--- Library/Library/Form1.Designer.cs
+++ Library/Library/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -42,32 +43,136 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+=======
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblDesigned = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
             // 
-            // textBox1
+            // btnLogin
             // 
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(473, 320);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 30);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+>>>>>>> Stashed changes
+            // 
+            // txtID
+            // 
+<<<<<<< Updated upstream
             this.textBox1.Location = new System.Drawing.Point(352, 164);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
+=======
+            this.txtID.ForeColor = System.Drawing.Color.Silver;
+            this.txtID.Location = new System.Drawing.Point(409, 190);
+            this.txtID.MaximumSize = new System.Drawing.Size(400, 400);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(230, 30);
+            this.txtID.TabIndex = 1;
+            this.txtID.Text = "ID";
+            this.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtID.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.txtID.Leave += new System.EventHandler(this.textBox1_Leave);
+>>>>>>> Stashed changes
             // 
-            // textBox2
+            // panel1
             // 
+<<<<<<< Updated upstream
             this.textBox2.Location = new System.Drawing.Point(352, 212);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
+=======
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.lblDesigned);
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Location = new System.Drawing.Point(0, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(282, 456);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.White;
+            this.lblWelcome.Location = new System.Drawing.Point(3, 142);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(267, 156);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome to the \r\nLibrary \r\nManagement \r\nSystem";
+            // 
+            // lblDesigned
+            // 
+            this.lblDesigned.AutoSize = true;
+            this.lblDesigned.ForeColor = System.Drawing.Color.White;
+            this.lblDesigned.Location = new System.Drawing.Point(3, 393);
+            this.lblDesigned.Name = "lblDesigned";
+            this.lblDesigned.Size = new System.Drawing.Size(97, 52);
+            this.lblDesigned.TabIndex = 1;
+            this.lblDesigned.Text = "Designed by:\r\nPieter Pienaar \r\nGideon Vermeulen \r\nFrederik Knoetze";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblLogin.Location = new System.Drawing.Point(368, 100);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(329, 37);
+            this.lblLogin.TabIndex = 4;
+            this.lblLogin.Text = "Login to your Account";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.ForeColor = System.Drawing.Color.Silver;
+            this.txtPassword.Location = new System.Drawing.Point(409, 254);
+            this.txtPassword.MaximumSize = new System.Drawing.Size(400, 400);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(230, 30);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+>>>>>>> Stashed changes
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+<<<<<<< Updated upstream
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+=======
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnLogin);
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+>>>>>>> Stashed changes
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,9 +180,19 @@
 
         #endregion
 
+<<<<<<< Updated upstream
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+=======
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblDesigned;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+>>>>>>> Stashed changes
     }
 }
 
--- Library/Library/Form1.cs
+++ Library/Library/Form1.cs
@@ -19,7 +19,120 @@ namespace Library
 
         private void Form1_Load(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             //test
+=======
+
+            //check to see if database admin exsists if not create it
+            if (File.Exists("admin.sqlite"))
+            {
+                // MessageBox.Show("file exsisst");
+                //test mesage
+            } else
+            {
+                SQLiteConnection.CreateFile("admin.sqlite");
+            }
+            //open connection to database so it can be accsesed
+            string connectionstring = "Data Source=admin.sqlite;Version=3;Encryption=SQLiteCrypt";
+            SQLiteConnection myconn = new SQLiteConnection(connectionstring);
+            myconn.Open();
+
+
+            string sql = "";
+            SQLiteCommand mycmd = new SQLiteCommand(sql, myconn);
+
+
+
+            //try
+            //{
+            //    sql = "Create table test (name varchar(20), numbers int)";
+            //    mycmd = new SQLiteCommand(sql, myconn);
+            //    mycmd.ExecuteNonQuery();
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("table exsisted");
+            //    throw;
+            //}
+            //for this to work will need more code.
+
+            //creates table is it does not exsist
+            sql = "Create table if not exists test (name varchar(20), numbers int)";
+            mycmd = new SQLiteCommand(sql, myconn);
+            mycmd.ExecuteNonQuery();
+
+
+            //there is enough dummy data
+            sql = "Insert into test (name,numbers) values ('testname',9000)";
+            mycmd = new SQLiteCommand(sql, myconn);
+            mycmd.ExecuteNonQuery();
+            sql = "Insert into test (name,numbers) values ('testname1',9001)";
+            mycmd = new SQLiteCommand(sql, myconn);
+            mycmd.ExecuteNonQuery();
+            sql = "Insert into test (name,numbers) values ('testname2',9002)";
+            mycmd = new SQLiteCommand(sql, myconn);
+            mycmd.ExecuteNonQuery();
+            sql = "Insert into test (name,numbers) values ('testname3',9003)";
+            mycmd = new SQLiteCommand(sql, myconn);
+            mycmd.ExecuteNonQuery();
+
+
+            // SQLiteDataReader reader = mycmd.ExecuteReader();
+            //get data into datagrid
+            sql = "select * from test";
+            mycmd = new SQLiteCommand(sql, myconn);
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(mycmd);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            //display dummy data to test
+            //while (reader.Read())
+            //{
+            //    MessageBox.Show("Name: " + reader["name"] + "\tScore: " + reader["numbers"]);
+            //}
+
+            ////////Gideon CODE\\\\\\\\\
+            sql = "DROP TABLE IF EXISTS tblReadersTest";
+
+            using (SQLiteCommand cmdDeleteTable = new SQLiteCommand(sql, myconn))
+            {
+                cmdDeleteTable.ExecuteNonQuery();
+                MessageBox.Show("TBL deleted");
+            }
+
+
+            sql = "CREATE TABLE IF NOT EXISTS tblReadersTest (ID  VARCHAR(30), ReaderName VARCHAR(30))";
+            using (SQLiteCommand cmdReaders = new SQLiteCommand(sql, myconn)) // Renamed to cmdReaders
+            {
+                cmdReaders.ExecuteNonQuery();
+            }
+
+            // Insert dummy data into 'tblReaders'
+            Reader Gideon = new Reader("012345", "Gideon");
+
+            // Prepare the SQL query to insert the Reader object
+            sql = @"INSERT INTO tblReadersTest (ID, ReaderName) 
+               VALUES (@ID, @ReaderName)";
+
+            // Use a parameterized query to avoid SQL injection
+            using (SQLiteCommand cmdInsertReader = new SQLiteCommand(sql, myconn))
+            {
+                // Add parameters for ID and ReaderName
+                cmdInsertReader.Parameters.AddWithValue("@ID", Gideon.ID);
+                cmdInsertReader.Parameters.AddWithValue("@ReaderName", Gideon.Name);
+
+                // Execute the query
+                cmdInsertReader.ExecuteNonQuery();
+            }
+            // Fetch data from 'tblReaders' table and display in DataGridView2
+            sql = "SELECT * FROM tblReadersTest";
+            using (SQLiteCommand cmdSelectReaders = new SQLiteCommand(sql, myconn)) // Renamed to cmdSelectReaders
+            {
+                SQLiteDataAdapter adapterReaders = new SQLiteDataAdapter(cmdSelectReaders);
+                DataTable readerData = new DataTable();
+                adapterReaders.Fill(readerData);
+            }
+>>>>>>> Stashed changes
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,6 +140,7 @@ namespace Library
             Random random = new Random();
             for (int i = 0; i < 3; i++)
             {
+<<<<<<< Updated upstream
                 int TempID = random.Next(10000,99999);
                 Reader Test = new Reader(TempID.ToString(),"Test");
                 
@@ -36,12 +150,69 @@ namespace Library
             {
                 string testID = textBox1.Text;
                 string testName = textBox2.Text;
+=======
+                // Add parameters with the entered values
+                command.Parameters.AddWithValue("@EnteredID", txtID.Text);
+                command.Parameters.AddWithValue("@EnteredName", txtPassword.Text);
+>>>>>>> Stashed changes
 
                 if (testID == reader.ID && testName == reader.Name)
                 {
 
                     MessageBox.Show($"ID: {reader.ID}, Name: {reader.Name}");
                 }
+<<<<<<< Updated upstream
+=======
+                catch (Exception ex)
+                {
+                    // Handle any errors that might have occurred
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "ID")
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (txtID.Text == "")
+            {
+                txtID.Text = "ID";
+                txtID.ForeColor = Color.Silver;
+            }
+            else
+            {
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Name")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Name";
+                txtPassword.ForeColor = Color.Silver;
+            }
+            else
+            {
+                txtPassword.ForeColor = Color.Black;
+>>>>>>> Stashed changes
             }
         }
     }
