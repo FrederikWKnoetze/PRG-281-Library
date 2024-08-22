58fd198b6f23f9ae7fa138a16ab2ff7258f238ac
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-08-22 17:03:32 +0200

    main menu

---
+++ Library/Library/frmMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+            StartPosition=FormStartPosition.CenterScreen;
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
