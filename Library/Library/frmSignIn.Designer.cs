35ac9eb183eaec3eb87ce1d51066ec6a018cb219
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-08-22 16:42:19 +0200

    splash screen

---
+++ Library/Library/frmSignIn.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Library
+{
+    partial class frmSignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.edtID = new System.Windows.Forms.TextBox();
+            this.edtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignIn.ForeColor = System.Drawing.Color.Black;
+            this.btnSignIn.Location = new System.Drawing.Point(283, 273);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(118, 40);
+            this.btnSignIn.TabIndex = 0;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            this.btnSignIn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // edtID
+            // 
+            this.edtID.ForeColor = System.Drawing.Color.Silver;
+            this.edtID.Location = new System.Drawing.Point(283, 131);
+            this.edtID.Name = "edtID";
+            this.edtID.Size = new System.Drawing.Size(118, 20);
+            this.edtID.TabIndex = 1;
+            this.edtID.Text = "ID";
+            this.edtID.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.edtID.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // edtPassword
+            // 
+            this.edtPassword.ForeColor = System.Drawing.Color.Silver;
+            this.edtPassword.Location = new System.Drawing.Point(283, 222);
+            this.edtPassword.Name = "edtPassword";
+            this.edtPassword.Size = new System.Drawing.Size(118, 20);
+            this.edtPassword.TabIndex = 2;
+            this.edtPassword.Text = "Password";
+            this.edtPassword.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.edtPassword.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Black;
+            this.lblPassword.Location = new System.Drawing.Point(287, 171);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(106, 25);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(304, 94);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(68, 25);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            // 
+            // frmSignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 431);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.edtPassword);
+            this.Controls.Add(this.edtID);
+            this.Controls.Add(this.btnSignIn);
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.Name = "frmSignIn";
+            this.Text = "Sign in";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmSignIn_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.TextBox edtID;
+        private System.Windows.Forms.TextBox edtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ Library/Library/frmSplash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class frmSplash : Form
+    {
+        private static System.Windows.Forms.Timer timer;
+        public frmSplash()
+        {
+            InitializeComponent();
+            //start form in middle of screen
+            StartPosition = FormStartPosition.CenterScreen;
+
+
+            //show form for 1.5 secs then show sign in form
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1500;
+            timer.Tick += ontimeout;
+            timer.Start();
+        }
+        private void frmSplash_Load(object sender, EventArgs e)
+        {
+
+
+            
+        }
+        private void ontimeout(Object sender, EventArgs e)
+        {
+            timer.Stop();
+            this.Hide();
+            frmSignIn f = new frmSignIn();
+            f.Show();
+            
+            
+        }
+    }
+}
