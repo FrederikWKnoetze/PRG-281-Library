2941abfe8f357f49e897abbbafd9ff1b3782e73d
Artix <kwevoel727@gmail.com>
2024-08-24 20:10:43 +0200

    button stuff

--- Library/Library/Borrow Book.Designer.cs
+++ Library/Library/Borrow Book.Designer.cs
@@ -98,6 +98,7 @@
             // 
             // btnBack
             // 
+            this.btnBack.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.btnBack.Location = new System.Drawing.Point(337, 401);
