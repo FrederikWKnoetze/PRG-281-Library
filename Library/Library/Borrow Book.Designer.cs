899094363dbadc6a7f65a62500544908aebe9489
GideonVermeulen <63670713+GideonVermeulen@users.noreply.github.com>
2024-08-25 01:48:48 +0200

    add books
    addbooks done with validation and db code, need visual stuff but 99% of the thinking code is done

--- Library/Library/Borrow Book.Designer.cs
+++ Library/Library/Borrow Book.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Library
+﻿using System.Drawing;
+
+namespace Library
 {
     partial class Borrow_Book
     {
@@ -37,8 +39,17 @@
             this.lblBorrowBook = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.edtISBN = new System.Windows.Forms.TextBox();
+            this.btnAddtoList = new System.Windows.Forms.Button();
+            this.edtReaderId = new System.Windows.Forms.TextBox();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            this.pnlISBN = new System.Windows.Forms.Panel();
+            this.pnlReaderID = new System.Windows.Forms.Panel();
+            this.richBookList = new System.Windows.Forms.RichTextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlISBN.SuspendLayout();
+            this.pnlReaderID.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -115,12 +126,88 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // edtISBN
+            // 
+            this.edtISBN.ForeColor = System.Drawing.Color.Silver;
+            this.edtISBN.Location = new System.Drawing.Point(1, 1);
+            this.edtISBN.Name = "edtISBN";
+            this.edtISBN.Size = new System.Drawing.Size(121, 20);
+            this.edtISBN.TabIndex = 9;
+            this.edtISBN.Text = "ISBN";
+            this.edtISBN.Enter += new System.EventHandler(this.edtISBN_Enter);
+            this.edtISBN.Leave += new System.EventHandler(this.edtISBN_Leave);
+            // 
+            // btnAddtoList
+            // 
+            this.btnAddtoList.Location = new System.Drawing.Point(339, 273);
+            this.btnAddtoList.Name = "btnAddtoList";
+            this.btnAddtoList.Size = new System.Drawing.Size(121, 23);
+            this.btnAddtoList.TabIndex = 10;
+            this.btnAddtoList.Text = "Add Book To List";
+            this.btnAddtoList.UseVisualStyleBackColor = true;
+            this.btnAddtoList.Click += new System.EventHandler(this.btnAddtoList_Click);
+            // 
+            // edtReaderId
+            // 
+            this.edtReaderId.ForeColor = System.Drawing.Color.Silver;
+            this.edtReaderId.Location = new System.Drawing.Point(1, 1);
+            this.edtReaderId.Name = "edtReaderId";
+            this.edtReaderId.Size = new System.Drawing.Size(121, 20);
+            this.edtReaderId.TabIndex = 11;
+            this.edtReaderId.Text = "Reader ID";
+            this.edtReaderId.Enter += new System.EventHandler(this.edtReaderId_Enter);
+            this.edtReaderId.Leave += new System.EventHandler(this.edtReaderId_Leave);
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCheckOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckOut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnCheckOut.Location = new System.Drawing.Point(337, 344);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(121, 37);
+            this.btnCheckOut.TabIndex = 12;
+            this.btnCheckOut.Text = "Check Out";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
+            this.btnCheckOut.Click += new System.EventHandler(this.btnCheckOut_Click);
+            // 
+            // pnlISBN
+            // 
+            this.pnlISBN.Controls.Add(this.edtISBN);
+            this.pnlISBN.Location = new System.Drawing.Point(339, 236);
+            this.pnlISBN.Name = "pnlISBN";
+            this.pnlISBN.Size = new System.Drawing.Size(123, 22);
+            this.pnlISBN.TabIndex = 13;
+            // 
+            // pnlReaderID
+            // 
+            this.pnlReaderID.Controls.Add(this.edtReaderId);
+            this.pnlReaderID.Location = new System.Drawing.Point(339, 310);
+            this.pnlReaderID.Name = "pnlReaderID";
+            this.pnlReaderID.Size = new System.Drawing.Size(123, 22);
+            this.pnlReaderID.TabIndex = 14;
+            // 
+            // richBookList
+            // 
+            this.richBookList.Location = new System.Drawing.Point(514, 236);
+            this.richBookList.Name = "richBookList";
+            this.richBookList.Size = new System.Drawing.Size(125, 144);
+            this.richBookList.TabIndex = 15;
+            this.richBookList.Text = "Book List: ";
+            this.richBookList.Visible = false;
+            this.richBookList.VisibleChanged += new System.EventHandler(this.richBookList_VisibleChanged);
+            // 
             // Borrow_Book
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnBack;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richBookList);
+            this.Controls.Add(this.btnAddtoList);
+            this.Controls.Add(this.pnlReaderID);
+            this.Controls.Add(this.pnlISBN);
+            this.Controls.Add(this.btnCheckOut);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.panel1);
             this.Name = "Borrow_Book";
@@ -129,6 +216,10 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlISBN.ResumeLayout(false);
+            this.pnlISBN.PerformLayout();
+            this.pnlReaderID.ResumeLayout(false);
+            this.pnlReaderID.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -142,5 +233,12 @@
         private System.Windows.Forms.Label lblBorrowBook;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox edtISBN;
+        private System.Windows.Forms.Button btnAddtoList;
+        private System.Windows.Forms.TextBox edtReaderId;
+        private System.Windows.Forms.Button btnCheckOut;
+        private System.Windows.Forms.Panel pnlISBN;
+        private System.Windows.Forms.Panel pnlReaderID;
+        private System.Windows.Forms.RichTextBox richBookList;
     }
 }
--- Library/Library/Borrow Book.cs
+++ Library/Library/Borrow Book.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Library
 {
+
     public partial class Borrow_Book : Form
     {
+        public static List<string> books = new List<string>();
         public Borrow_Book()
         {
             InitializeComponent();
@@ -34,6 +38,246 @@ namespace Library
         {
             lblDate.Text = DateTime.Now.ToShortDateString();
             lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void edtISBN_Enter(object sender, EventArgs e)
+        {
+            if (edtISBN.Text == "ISBN")
+            {
+                edtISBN.Text = "";
+                edtISBN.ForeColor = Color.Black;
+            }
+        }
+
+        private void edtISBN_Leave(object sender, EventArgs e)
+        {
+            if (edtISBN.Text == "")
+            {
+                edtISBN.Text = "ISBN";
+                edtISBN.ForeColor = Color.Silver;
+            }
+            else
+            {
+                edtISBN.ForeColor = Color.Black;
+            }
+        }
+
+        private void edtReaderId_Enter(object sender, EventArgs e)
+        {
+            if (edtReaderId.Text == "Reader ID")
+            {
+                edtReaderId.Text = "";
+                edtReaderId.ForeColor = Color.Black;
+            }
+        }
+
+        private void edtReaderId_Leave(object sender, EventArgs e)
+        {
+            if (edtReaderId.Text == "")
+            {
+                edtReaderId.Text = "Reader ID";
+                edtReaderId.ForeColor = Color.Silver;
+            }
+            else
+            {
+                edtReaderId.ForeColor = Color.Black;
+            }
+        }
+
+        private void btnCheckOut_Click(object sender, EventArgs e)
+        {
+            validUser();
+        }
+        public void ValidateISBN()
+        {
+            string TempISBN = edtISBN.Text;
+            string nums = "0123456789";
+            int countCorrectNums = 0;
+            bool isbnCorrect = false;
+            if (TempISBN.Length == 13)//loop to check if if all 13 characters is a number
+            {
+                for (int i = 0; i < TempISBN.Length; i++)
+                {
+                    if (nums.Contains(TempISBN[i]))
+                    {
+                        countCorrectNums += 1;
+                    }
+                }
+                if (countCorrectNums == 13) // Check if all characters are digits
+                {
+                    try
+                    {
+                        // Check if the connection is properly initialized and not open already
+                        if (DataHandler.myconn == null)
+                        {
+                            DataHandler.myconn = new SQLiteConnection("Data Source=LibraryData.sqlite;Version=3;");
+                        }
+
+                        // Open the connection only if it is closed
+                        if (DataHandler.myconn.State == System.Data.ConnectionState.Closed)
+                        {
+                            DataHandler.myconn.Open();
+                        }
+
+
+                        //Just some testing to se if it works
+                        string sql = "SELECT * FROM tblBooks WHERE isbn = @ISBN";
+                        SQLiteCommand cmd = new SQLiteCommand(sql, DataHandler.myconn);
+                        cmd.CommandText = sql;
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("@ISBN", edtISBN.Text);
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+
+                        string result = "";
+                        bool isBorrowed = false;
+                        while (reader.Read())
+                        {
+                            result += $"Book ID: {reader["bookID"]}, ISBN: {reader["isbn"]}, Title: {reader["title"]}, Author: {reader["author"]}, Borrowed: {reader["borrowed"]}\n";
+                            if (reader["borrowed"].ToString() == "1")
+                            {
+                                isBorrowed = true;
+                            }
+                        }
+
+                        // Display results
+                        if (string.IsNullOrEmpty(result))
+                        {
+                            MessageBox.Show("This book does not exist");
+                            pnlISBN.BackColor = Color.Red;
+                        }
+                        else if (isBorrowed == true)
+                        {
+                            MessageBox.Show("This book is already borrowed.");
+                            pnlISBN.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            MessageBox.Show(result);
+                            isbnCorrect = true;
+                            pnlISBN.BackColor = Color.White;
+                            richBookList.Visible = true;
+                            richBookList.AppendText(edtISBN.Text + Environment.NewLine);
+                            edtISBN.Text = "";
+                            books.Add(edtISBN.Text);
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Display the error
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                    finally
+                    {
+                        // Closing the conn string for coding standards
+                        if (DataHandler.myconn != null && DataHandler.myconn.State == System.Data.ConnectionState.Open)
+                        {
+                            DataHandler.myconn.Close();
+                        }
+                    }
+                }
+            }
+            if (isbnCorrect == false)
+            {
+                pnlISBN.BackColor = Color.Red;
+            }
+        }
+
+        private void btnAddtoList_Click(object sender, EventArgs e)
+        {
+            ValidateISBN();
+        }
+
+        private void richBookList_VisibleChanged(object sender, EventArgs e)
+        {
+            richBookList.AppendText(Environment.NewLine);
+        }
+        public void markAsBorrowed()
+        {
+            if (books.Count >= 1)
+            {
+                try
+                {
+                    if (DataHandler.myconn == null)
+                    {
+                        DataHandler.myconn = new SQLiteConnection("Data Source=LibraryData.sqlite;Version=3;");
+                    }
+
+                    if (DataHandler.myconn.State == System.Data.ConnectionState.Closed)
+                    {
+                        DataHandler.myconn.Open();
+                    }
+                    foreach (var book in books)
+                    {
+                        string updateSql = "UPDATE tblBooks SET borrowed = 0 WHERE isbn = @ISBN";
+                        SQLiteCommand updateCmd = new SQLiteCommand(updateSql, DataHandler.myconn);
+                        updateCmd.Parameters.Clear();
+                        updateCmd.Parameters.AddWithValue("@ISBN", book);
+                        updateCmd.ExecuteNonQuery();
+                        MessageBox.Show("The book has been successfully updated to borrowed.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+                finally
+                {
+                    if (DataHandler.myconn != null && DataHandler.myconn.State == System.Data.ConnectionState.Open)
+                    {
+                        DataHandler.myconn.Close();
+                    }
+                }
+
+            }
+        }
+        public void validUser()
+        {
+            try
+            {
+                if (DataHandler.myconn == null)
+                {
+                    DataHandler.myconn = new SQLiteConnection("Data Source=LibraryData.sqlite;Version=3;");
+                }
+
+                if (DataHandler.myconn.State == System.Data.ConnectionState.Closed)
+                {
+                    DataHandler.myconn.Open();
+                }
+
+                // SQL query to check if the user exists
+                string sql = "SELECT COUNT(*) FROM tblReaders WHERE readerID = @readerID";
+                SQLiteCommand cmd = new SQLiteCommand(sql, DataHandler.myconn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@readerID", edtReaderId.Text);
+
+                
+                int userCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+                // If userCount is greater than 0, it means the user exists
+                if (userCount > 0)
+                {
+                    MessageBox.Show("Valid user found.");
+                    markAsBorrowed();
+                }
+                else 
+                {
+                    MessageBox.Show("User is not found please make an Account");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                // Closing the connection for coding standards
+                if (DataHandler.myconn != null && DataHandler.myconn.State == System.Data.ConnectionState.Open)
+                {
+                    DataHandler.myconn.Close();
+                }
+            }
         }
     }
 }
